58ba8741508366e902f745a1b4c4ef155834f261
Dipankar Biswas <423829+dbiswas@users.noreply.github.com>
2022-02-11 19:56:53 -0800

    Added some additional imports

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------------
+# Import requirement modules
+# ---------------------------------
+import xlwings as xw
+import pandas as pd
+import numpy as np
+import time
+import datetime as dt
+import requests
+import json
+import urllib3
+from pathlib import Path
 import flask
-import os
-import datetime
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
